54bd814d77a0dc0e7f5af6b70bfd382632e8906b
Kelvin Lau <kclau95@gmail.com>
2016-07-12 00:07:24 +1200

    change 'dist' folder to 'www'

--- gulpfile.js
+++ gulpfile.js
@@ -11,31 +11,31 @@ gulp.task('default', ['build']);
 gulp.task('build', ['html', 'scripts', 'styles', 'fonts']);
 
 gulp.task('html', function(){
-  return gulp.src('src/**/*.html').pipe(gulp.dest('dist'));
+  return gulp.src('src/**/*.html').pipe(gulp.dest('www'));
 });
 
 gulp.task('scripts', ['javascript', 'typescript']);
 
 gulp.task('javascript', function(){
-  return gulp.src('src/scripts/**/*.js').pipe(gulp.dest('dist/scripts'));
+  return gulp.src('src/scripts/**/*.js').pipe(gulp.dest('www/scripts'));
 });
 gulp.task('typescript', function(){
-  return proj.src().pipe(ts(proj)).js.pipe(gulp.dest('dist/scripts'));
+  return proj.src().pipe(ts(proj)).js.pipe(gulp.dest('www/scripts'));
 });
 
 gulp.task('styles', ['css', 'sass']);
 
 gulp.task('css', function(){
-  return gulp.src('src/css/**/*.css').pipe(gulp.dest('dist/css'));
+  return gulp.src('src/css/**/*.css').pipe(gulp.dest('www/css'));
 });
 gulp.task('sass', function(){
-  return gulp.src('src/css/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('dist/css'));
+  return gulp.src('src/css/**/*.scss').pipe(sass().on('error', sass.logError)).pipe(gulp.dest('www/css'));
 });
 
 gulp.task('fonts', function(){
-  return gulp.src('src/fonts/**/*.*').pipe(gulp.dest('dist/fonts'));
+  return gulp.src('src/fonts/**/*.*').pipe(gulp.dest('www/fonts'));
 });
 
 gulp.task('clean', function(){
-  return del(['dist/**/*']);
+  return del(['www/**/*']);
 });
