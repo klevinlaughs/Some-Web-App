5609669dbdccf53afe4032991827ccbb7ad8452a
Kelvin Lau <kclau95@gmail.com>
2016-09-15 04:01:21 +1200

    Serve file

--- app/main.js
+++ app/main.js
@@ -1,4 +1,16 @@
-function hello(world) {
-    console.log(`Hello from ${world}`);
-}
-hello("Klevin");
+'use strict';
+const express = require('express');
+const http = require('http');
+const app = express();
+var PORT = process.env.PORT || 6969;
+let _root = process.cwd();
+let _nodeModules = '/node_modules/';
+let _clientFiles = '/app/';
+app.use(express.static(_root + _nodeModules));
+app.use(express.static(_root + _clientFiles));
+app.use('/', express.static(_root + _clientFiles + "index.html"));
+http.createServer(app)
+    .listen(PORT, () => {
+    console.log(`up and running on port: ${PORT}`);
+    console.log(`enviroment: ${process.env.NODE_ENV}`);
+});
--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,29 @@
-function hello(world: string) : void {
-    console.log(`Hello from ${world}`);
-}
+/// <reference path="../typings/index.d.ts" />
 
-hello("Klevin");
+'use strict'
+
+import * as express from 'express';
+import * as os from 'os';
+import * as http from 'http';
+
+const app = express();
+
+var PORT = process.env.PORT || 6969;
+
+let _root = process.cwd();
+let _nodeModules = '/node_modules/';
+let _clientFiles = '/app/'
+
+app.use(express.static(_root + _nodeModules));
+app.use(express.static(_root + _clientFiles));
+
+app.use('/', express.static(_root + _clientFiles + "index.html"));
+// app.get('/', function(req:any, res:any){
+//     res.sendFile(_root + _clientFiles + "index.html");
+// });
+
+http.createServer(app)
+    .listen(PORT, () => {
+      console.log(`up and running @: ${os.hostname()} on port: ${PORT}`);
+      console.log(`enviroment: ${process.env.NODE_ENV}`);
+    });
